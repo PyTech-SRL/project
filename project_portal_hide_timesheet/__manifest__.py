f4cb5ac9e42929fba70cb7cb5c1fad3a43aea4e1
Simone Rubino <simone.rubino@pytech.it>
2026-08-13 12:38:38 +0200

    [MIG] project_portal_hide_timesheet: Migration to 18.0

--- project_portal_hide_timesheet/__manifest__.py
+++ project_portal_hide_timesheet/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Project Portal Hide Timesheet",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "depends": [
         "hr_timesheet",
         "portal",
@@ -10,9 +10,9 @@
     "author": "PyTech SRL, Odoo Community Association (OCA)",
     "category": "Project",
     "license": "AGPL-3",
-    "data": ["views/project_task_templates.xml", "views/timesheet_sheet_templates.xml"],
-    "installable": True,
-    "application": False,
+    "data": [
+        "views/timesheet_sheet_templates.xml",
+    ],
     "post_init_hook": "post_init_hook",
     "uninstall_hook": "uninstall_hook",
 }
